aef1b0edd7657d57f493f26eab36f601070899b3
Raz <stsraz@gmail.com>
2014-11-07 23:18:09 -0700

    SBA Tech Tracker Rewrite Test Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+		<meta charset="utf-8">
+
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>new_file</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by Joe Mamma
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
